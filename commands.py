d8dbed99d48e9e6be40ccf8b55f3ad294d815b14
ferrynurqadar <67650329+marmuthandsome@users.noreply.github.com>
2024-08-02 00:24:23 +0700

    Fix Bug

--- commands.py
+++ commands.py
@@ -137,14 +137,14 @@ def run_evilwinrm_hash(url, username, hash):
 
 def run_crackmapexec_smb_password(url, username, password):
     try:
-        command = f"sudo crackmapexec smb {url} -u {username} -p '{password}' --continue-on-success"
+        command = f"sudo crackmapexec smb {url} -u {username} -p '{password}'"
         subprocess.run(command, shell=True)
     except Exception as e:
         print(f"{RED}Error running crackmapexec: {e}{RESET}")
 
 def run_crackmapexec_smb_hash(url, username, hash):
     try:
-        command = f"sudo crackmapexec smb {url} -u {username} -H {hash} --continue-on-success"
+        command = f"sudo crackmapexec smb {url} -u {username} -H {hash}"
         subprocess.run(command, shell=True)
     except Exception as e:
         print(f"{RED}Error running crackmapexec: {e}{RESET}")
